8f1fcabdc4511a892a76909639d186bb89d430aa
Ariel Leyva <aleyva1509@gmail.com>
2023-12-30 08:20:15 -0600

    feat: time formatting and printing

--- main.go
+++ main.go
@@ -5,27 +5,34 @@ import (
 	"time"
 )
 
+func formatTime(seconds int) string {
+	minutes := seconds / 60
+	seconds = seconds % 60
+	return fmt.Sprintf("%02d:%02d", minutes, seconds)
+}
+
 func main() {
 	// creating a 1 second ticker
 	ticker := time.NewTicker(time.Second * 1)
+	twentyfiveminutes := 3
+
 	done := make(chan bool) // channel to receive signal when to stop
-	i := 0
 	// every second, print "tick"
 	go func() { // create a goroutine to run this function
 		for {
 			select {
 			case <-done:
+				ticker.Stop()
 				return
 			case <-ticker.C:
-				fmt.Println("tick", i)
-				i++
+				twentyfiveminutes--
+				fmt.Println(formatTime(twentyfiveminutes))
+				if twentyfiveminutes == 0 {
+					done <- true
+				}
 			}
 		}
 	}()
 
-	// wait for 5 seconds
-	time.Sleep(time.Second * 5)
-	ticker.Stop()
-	done <- true // set channel to true to stop the goroutine
-	fmt.Println("Ticker stopped")
+	<-done // block here until we receive the done signal
 }
